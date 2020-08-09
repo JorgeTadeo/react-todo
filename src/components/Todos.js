96a10ec7ce1c772c6a740d7340593ad7940be6b8
JorgeTadeo <jorge_tadeo@icloud.com>
2020-08-09 15:06:02 -0700

    added states and components TodoItem and Todos.

---
+++ src/components/Todos.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import TodoItem from './TodoItem';
+import Proptypes from 'prop-types';
+
+class Todos extends Component {
+  render() {
+      return this.props.todos.map((todo) =>(
+      <TodoItem key={todo.id} todo={todo}/>
+      ));
+    }
+}
+
+//PropTypes
+Todos.propTypes = {
+    todos: Proptypes.array.isRequired
+}
+
+export default Todos;
