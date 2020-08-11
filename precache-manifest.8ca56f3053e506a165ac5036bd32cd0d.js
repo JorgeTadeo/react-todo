1338d80b94814549f3f12703118099763ae52a85
JorgeTadeo <jorge_tadeo@icloud.com>
2020-08-10 17:17:01 -0700

    Updates

---
+++ precache-manifest.8ca56f3053e506a165ac5036bd32cd0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af38e249c2059ff7dbc12889eedceb11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbdccfe374c43965a79c",
+    "url": "/static/css/main.defc0d43.chunk.css"
+  },
+  {
+    "revision": "1206d61605dec6967823",
+    "url": "/static/js/2.456f13fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.456f13fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbdccfe374c43965a79c",
+    "url": "/static/js/main.a477d20e.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
